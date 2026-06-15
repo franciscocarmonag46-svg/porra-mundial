980aec114594a03ff0586a60033bf9310c28a57f
franciscocarmonag46-svg <franciscocarmonag46@gmail.com>
2026-06-16 00:10:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -295,6 +295,7 @@ def admin():
     return render_template("admin.html", matches=rows)
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0")
